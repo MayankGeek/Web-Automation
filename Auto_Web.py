b369ec3205aa27285712c9d8b1f8efaad9a40c69
Mayank <msharma.onetriple9@gmail.com>
2020-03-05 20:18:53 +0530

    Added more features

--- Auto_Web.py
+++ Auto_Web.py
@@ -1,4 +1,5 @@
-#Need to add more features
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 def convert(url):
     if url.startswith('http://www.'):
         return 'http://' + url[len('http://www.'):]
@@ -9,6 +10,13 @@ def convert(url):
     return url 
 url=input("Enter a website name/url: ")
 print(convert(url))
-from selenium import webdriver
-driver=webdriver.Chrome()  
+username = input("Enter your email-id: ")
+password = input("Enter your password: ")
+driver = webdriver.Chrome()
 driver.get(convert(url))
+element = driver.find_element_by_id("email")
+element.send_keys(user_name)
+element = driver.find_element_by_id("pass")
+element.send_keys(password)
+element.send_keys(Keys.RETURN)
+element.close()
