6700ff5045ac270203b9aa6def43a3f533b5b1cd
Mayank <msharma.onetriple9@gmail.com>
2020-03-07 21:43:04 +0530

    Added security for password using getpass

--- Auto_Web.py
+++ Auto_Web.py
@@ -1,23 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-def convert(url):
-    if url.startswith('http://www.'):
-        return 'http://' + url[len('http://www.'):]
-    if url.startswith('www.'):
-        return 'https://' + url[len('www.'):]
-    if not url.startswith('https://'):
-        return 'https://www.' + url+".com"
-    return url 
-url=input("Enter a website name/url: ")
-print(convert(url))
-username = input("Enter your email-id: ")
-password = input("Enter your password: ")
+from getpass import getpass
+print("Facebook login from terminal")
+username = input("Enter your facebook username: ")
+password = getpass("Enter your facebook password: ")
 driver = webdriver.Chrome()
-driver.get(convert(url))
+driver.get("https://www.facebook.com")
 element = driver.find_element_by_id("email")
 element.send_keys(username)
 element = driver.find_element_by_id("pass")
 element.send_keys(password)
 element.send_keys(Keys.RETURN)
 element.close()
-
